7462b48138627ad50d9f8c640b7f5e61a8de24f0
Facundo Martin <facundomartin98@gmail.com>
2025-10-28 18:34:39 -0300

    chore(ai): integrate and test different ai providers via vercel ai sdk

--- src/server/inngest/functions.ts
+++ src/server/inngest/functions.ts
@@ -1,10 +1,9 @@
 import { inngest } from "@/lib/inngest";
-import { db } from "../db";
-import { workflows } from "../db/schema";
-import { generateText } from "ai";
-import { google } from "@ai-sdk/google";
 
-import { createGoogleGenerativeAI } from "@ai-sdk/google";
+import { generateText } from "ai";
+import { openai } from "@ai-sdk/openai";
+import { google } from "@ai-sdk/google";
+import { anthropic } from "@ai-sdk/anthropic";
 
 export const execute = inngest.createFunction(
   { id: "execute-ai" },
@@ -16,11 +15,37 @@ export const execute = inngest.createFunction(
     // });
     // console.log("Generated text:", text);
 
-    const { steps } = await step.ai.wrap("gemini-generate-text", generateText, {
-      model: google("gemini-2.5-flash"),
-      system: "You are a helpful assistant",
-      prompt: "What is 2 + 2?",
-    });
+    const { steps: geminiSteps } = await step.ai.wrap(
+      "gemini-generate-text",
+      generateText,
+      {
+        model: google("gemini-2.5-flash"),
+        system: "You are a helpful assistant",
+        prompt: "What is 2 + 2?",
+      }
+    );
+
+    const { steps: openaiSteps } = await step.ai.wrap(
+      "openai-generate-text",
+      generateText,
+      {
+        model: openai("gpt-4"),
+        system: "You are a helpful assistant",
+        prompt: "What is 2 + 2?",
+      }
+    );
+
+    const { steps: anthropicSteps } = await step.ai.wrap(
+      "anthropic-generate-text",
+      generateText,
+      {
+        model: anthropic("claude-sonnet-4-5"),
+        system: "You are a helpful assistant",
+        prompt: "What is 2 + 2?",
+      }
+    );
+
+    return { geminiSteps, openaiSteps, anthropicSteps };
 
     // await step.sleep("wait-a-moment", "1s");
     // await step.run("create-workflow", async () => {
